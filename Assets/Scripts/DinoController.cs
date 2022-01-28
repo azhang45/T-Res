91fbfe3b901ba4bc3724800aaaa076ad7a02c3a1
Alice Zhang <anz0063@gmail.com>
2022-01-27 20:24:45 -0500

    solved an oepsie and created more woepsies
    solved an oepsie and created more woepsies

--- Assets/Scripts/DinoController.cs
+++ Assets/Scripts/DinoController.cs
@@ -4,20 +4,43 @@ using UnityEngine;
 
 public class DinoController : MonoBehaviour
 {
-    //private float speed = 5.0f;
     private float jumpInput;
     private float jumpHeight = 10.0f;
+    private float timer = 0.0f;
+    private float jumpTime = 0.7f;
+    private float x;
+    private float z;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        x = transform.position.x;
+        z = transform.position.z;
     }
 
     // Update is called once per frame
     void Update()
     {
-        jumpInput = Input.GetAxis("Jump");
-        //transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.up * jumpHeight * jumpInput * Time.deltaTime);
+        timer += Time.deltaTime;
+
+        if(timer > 0.7){
+            jumpInput = Input.GetAxis("Jump");
+            transform.Translate(Vector3.up * jumpHeight * jumpInput * Time.deltaTime);
+            if (jumpInput > 0){
+                jumpTime -= Time.deltaTime;
+                if(jumpTime <= 0){
+                    jumpTime = 0.7f;
+                    timer = 0.0f;
+                } 
+            }
+            else{
+                jumpTime = 0.7f;
+            }
+        }
+        transform.localRotation = Quaternion.Euler(0, -90, 0);
+        if(transform.position.x != 990 || transform.position.z != 500){
+            transform.position = new Vector3(990, transform.position.y, 500);
+        }
+        
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class SpawnManager : MonoBehaviour
 {
@@ -14,8 +15,8 @@ public class SpawnManager : MonoBehaviour
     {
         for(int i = 0; i < 6; i++){
             Instantiate(cactusPrefab, GenerateSpawn(min, max), cactusPrefab.transform.rotation);
-            min -= 15;
-            max -= 15;
+            min -= 20;
+            max -= 20;
         }
         
     }
@@ -29,8 +30,11 @@ public class SpawnManager : MonoBehaviour
         cactusList = FindObjectsOfType<Cactus>();
         cactusCount = cactusList.Length;
         if(cactusCount < 6){
-            max = cactusList[(int)cactusCount-1].transform.position.x;
-            Instantiate(cactusPrefab, GenerateSpawn(min, max-20), cactusPrefab.transform.rotation);
+            float[] arr = {cactusList[0].transform.position.x, cactusList[1].transform.position.x, 
+                cactusList[2].transform.position.x, cactusList[3].transform.position.x, cactusList[4].transform.position.x};
+            //max = cactusList[(int)cactusCount-1].transform.position.x;
+            max = arr.Min();
+            Instantiate(cactusPrefab, GenerateSpawn(max-40, max-20), cactusPrefab.transform.rotation);
         }
     }
 }
