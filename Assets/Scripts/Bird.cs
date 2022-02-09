0fb98e97190ebe04670730c7af31fceac9dcde13
Alice Zhang <anz0063@gmail.com>
2022-02-09 12:40:07 -0500

    -skylar was here
    i am literally the best person to ever exist on this planter

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -2,21 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cactus : MonoBehaviour
+public class Bird : MonoBehaviour
 {
     public float speed;
-
     // Start is called before the first frame update
     void Start()
     {
-        speed = 8.0f;
+        speed = 10.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
         if(transform.position.x > 995){
             Destroy(gameObject);
         }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,15 +8,20 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject cactusPrefab;
     public float cactusCount;
+    public GameObject birdPrefab;
+    public float birdCount;
     private float max = 980;
     private float min = 970;
     private Cactus[] cactusList;
+    private Bird[] birdList;
     private bool accounted = false;
 
     public GameOverScreen GameOverScreen;
     int score = 0;
     float testTimer = 30;
     public ScoreText ScoreText;
+
+    public DinoController DinoController;
 
     public void GameOver(){
         GameOverScreen.Setup(score);
@@ -26,15 +31,16 @@ public class SpawnManager : MonoBehaviour
     void Start()
     {
         for(int i = 0; i < 6; i++){
-            Instantiate(cactusPrefab, GenerateSpawn(min, max), cactusPrefab.transform.rotation);
+            Instantiate(cactusPrefab, GenerateSpawn(min, max,0), cactusPrefab.transform.rotation);
+            Instantiate(birdPrefab, GenerateSpawn(min, max,7), birdPrefab.transform.rotation);
             min -= 20;
             max -= 20;
         }
         
     }
 
-    private Vector3 GenerateSpawn(float min, float max){
-        return new Vector3(Random.Range(min,max), 0, 500);
+    private Vector3 GenerateSpawn(float min, float max, float y){
+        return new Vector3(Random.Range(min,max), y, 500);
     }
     // Update is called once per frame
     void Update()
@@ -45,11 +51,19 @@ public class SpawnManager : MonoBehaviour
 
         cactusList = FindObjectsOfType<Cactus>();
         cactusCount = cactusList.Length;
+        birdList = FindObjectsOfType<Bird>();
+        birdCount = birdList.Length;
         float[] arr = null;
+        float[] brr = null;
 
         if(cactusCount >= 6){
             arr = new float[] {cactusList[0].transform.position.x, cactusList[1].transform.position.x, 
                 cactusList[2].transform.position.x, cactusList[3].transform.position.x, cactusList[4].transform.position.x, cactusList[5].transform.position.x};
+        }
+
+        if(birdCount >= 6){
+            brr = new float[] {birdList[0].transform.position.x, birdList[1].transform.position.x, 
+                birdList[2].transform.position.x, birdList[3].transform.position.x, birdList[4].transform.position.x, birdList[5].transform.position.x};
         }
         
 
@@ -58,19 +72,31 @@ public class SpawnManager : MonoBehaviour
             accounted = true;
         }
         
-        if (testTimer <= 0){
+        if (DinoController.over){
             GameOver();
             for(int i = 0; i < cactusCount; i++){
                 Destroy(cactusList[i]);
             }
+            for(int i = 0; i < birdCount; i++){
+                Destroy(birdList[i]);
+            }
         }
         
-        else if(cactusCount < 6){
-            float[] arr2 = {cactusList[0].transform.position.x, cactusList[1].transform.position.x, 
+        else if(cactusCount < 6 || birdCount < 6){
+            if (cactusCount < 6){
+                float[] arr2 = {cactusList[0].transform.position.x, cactusList[1].transform.position.x, 
                 cactusList[2].transform.position.x, cactusList[3].transform.position.x, cactusList[4].transform.position.x};
-            max = arr2.Min();
-            accounted = false;
-            Instantiate(cactusPrefab, GenerateSpawn(max-40, max-20), cactusPrefab.transform.rotation);
+                max = arr2.Min();
+                accounted = false;
+                Instantiate(cactusPrefab, GenerateSpawn(max-40, max-20, 0), cactusPrefab.transform.rotation);
+            }
+            if (birdCount < 6){
+                float[] brr2 = {birdList[0].transform.position.x, birdList[1].transform.position.x, 
+                birdList[2].transform.position.x, birdList[3].transform.position.x, birdList[4].transform.position.x};
+                max = brr2.Min();
+                Instantiate(birdPrefab, GenerateSpawn(max-40, max-20,7), birdPrefab.transform.rotation);
+            }
+            
         }
     }
 }
